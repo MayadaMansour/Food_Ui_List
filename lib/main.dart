cd72e7a7bf1af3ff4e0222a2e9d2b0fcb09193c8
MayadaMansour <mayadamansour1994@gmail.com>
2023-07-31 22:29:38 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:food_ui_list/virews/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Application',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
